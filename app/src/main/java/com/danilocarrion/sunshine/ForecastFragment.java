c841d04c0ed6c3a22d7c8ca7fb40f8f3e30c4f06
Danilo Carrion <adida948@hotmail.com>
2015-08-17 14:02:41 -0400

    2.04 Inflate menu Item(refresh)

--- app/src/main/java/com/danilocarrion/sunshine/ForecastFragment.java
+++ app/src/main/java/com/danilocarrion/sunshine/ForecastFragment.java
@@ -4,12 +4,14 @@ package com.danilocarrion.sunshine;
  * Created by dcarrion on 8/14/2015.
  * A placeholder fragment containing a simple view.
  */
-
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -25,11 +27,38 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public  class ForecastFragment extends Fragment {
+/**
+ * Encapsulates fetching the forecast and displaying it as a {@link ListView} layout.
+ */
+public class ForecastFragment extends Fragment {
 
-    ArrayAdapter<String> mForecastAdapter;
+    private ArrayAdapter<String> mForecastAdapter;
 
     public ForecastFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Add this line in order for this fragment to handle menu events
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.forecastfragment, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_refresh) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
@@ -64,6 +93,9 @@ public  class ForecastFragment extends Fragment {
         // Get a reference to the ListView, and attach this adapter to it.
         ListView forecastListView = (ListView) rootView.findViewById(R.id.listview_forecast);
         forecastListView.setAdapter(mForecastAdapter);
+
+        setHasOptionsMenu(true);
+
         return rootView;
     }
 
@@ -148,8 +180,6 @@ public  class ForecastFragment extends Fragment {
                     }
                 }
             }
-
-
 
 
             return null;
