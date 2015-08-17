015f101502360eebe18e13009ca5f69b99469e57
Danilo Carrion <adida948@hotmail.com>
2015-08-17 19:08:38 -0400

    3.0 Remove Logging

--- app/src/main/java/com/danilocarrion/sunshine/ForecastFragment.java
+++ app/src/main/java/com/danilocarrion/sunshine/ForecastFragment.java
@@ -202,10 +202,6 @@ public class ForecastFragment extends Fragment {
                 highAndLow = formatHighLows(high, low);
                 resultStrs[i] = day + " - " + description + " - " + highAndLow;
             }
-
-            for (String s : resultStrs) {
-                Log.v(LOG_TAG, "Forecast entry: " + s);
-            }
             return resultStrs;
 
         }
@@ -267,8 +263,6 @@ public class ForecastFragment extends Fragment {
                 // http://openweathermap.org/API#forecast
                 URL url = new URL(builtUri.toString());
 
-                Log.v(LOG_TAG, "Built URI: " + builtUri.toString());
-
                 // Create the request to OpenWeatherMap, and open the connection
                 urlConnection = (HttpURLConnection) url.openConnection();
                 urlConnection.setRequestMethod("GET");
@@ -297,8 +291,6 @@ public class ForecastFragment extends Fragment {
                     return null;
                 }
                 forecastJsonStr = buffer.toString();
-
-                Log.v(LOG_TAG, "Forecast Json String: " + forecastJsonStr);
 
             } catch (IOException e) {
                 Log.e(LOG_TAG, "Error", e);
