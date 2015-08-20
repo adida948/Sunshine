d224df38077923e916a42dc8a132f090da3aa625
Danilo Carrion <adida948@hotmail.com>
2015-08-20 15:44:44 -0400

    3.11.01 Clean code

--- app/src/main/java/com/danilocarrion/sunshine/MainActivity.java
+++ app/src/main/java/com/danilocarrion/sunshine/MainActivity.java
@@ -39,7 +39,7 @@ public class MainActivity extends AppCompatActivity {
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             //Click on the main Activity settings and send user to the SettingsActivity
-            startActivity(new Intent(getApplicationContext(),SettingsActivity.class ));
+            startActivity(new Intent(this,SettingsActivity.class ));
             return true;
         }
 
